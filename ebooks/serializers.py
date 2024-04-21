c7b9fc7691b3771efe50f3764ce2452a3b6215c4
Igor Carvalho <igorcodigo0@gmail.com>
2024-04-21 20:11:20 -0300

    Mudancas padroes realizadas

--- ebooks/serializers.py
+++ ebooks/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
-from ebooks.models import Ebook
+from ebooks.models import Ebook,Author,Genre
 
 class EbookModelSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Ebook
         fields = '__all__'
+
+    # def validate_num_pages(self,value):
+    #     if value < 5:
+    #         raise serializers.ValidationError('Precisa ter mais de 5 páginas')
+    
+class AuthorModelSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Author
+        fields = '__all__'
+        
+    def validate_first_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("O nome deve ter pelo menos 2 caracteres")
+        return value
+    
--- ebooks/views.py
+++ ebooks/views.py
@@ -1,11 +1,23 @@
 from rest_framework import generics
-from ebooks.models import Ebook
-from ebooks.serializers import EbookModelSerializer
+from ebooks.models import Ebook, Author
+from ebooks.serializers import EbookModelSerializer, AuthorModelSerializer
+
 
 class EbookCreateListView(generics.ListCreateAPIView):
     queryset = Ebook.objects.all()
     serializer_class = EbookModelSerializer
 
+
 class EbookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Ebook.objects.all()
     serializer_class = EbookModelSerializer
+
+
+class AuthorCreateListView(generics.ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorModelSerializer
+
+
+class AuthorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorModelSerializer
